df6935147524c01791f9b60602acab4b00c90692
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2022-06-20 23:58:49 -0300

    Update wifi-hack.py

--- wifi-hack.py
+++ wifi-hack.py
@@ -146,7 +146,7 @@ elif WH == 10:
     elif crearDic == 'n':
         pass    
 
-    print(" \033[1;37mIngrese la ruta del diccionario\033[0m (default: \033[1;37m/wordlist/fakeAP.txt\033[0m): ")
+    print("\n\033[1;37m Ingrese la ruta del diccionario\033[0m (default: \033[1;37m/wordlist/fakeAP.txt\033[0m): ")
     diccionario = str(input(" \033[1;32m>> \033[0;37m"))
     print("\n \033[1;31m[AVISO] \033[0;37mPresione \033[1;37m\033[1;37mCTRL + C \033[0;37mpara detener el ataque\033[0m")
     os.system("mdk3 {} b -f {} -a -s 1000 -c {}".format(interface,diccionario,channel))
