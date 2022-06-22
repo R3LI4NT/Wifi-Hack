6255d3d2a6362ebdbddb1f6f3b28b256d0b7650c
R3LI4NT <75953873+R3LI4NT@users.noreply.github.com>
2022-06-21 22:01:37 -0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,11 +3,13 @@
 dependencias() {
 
 	clear
-	printf "\e[1;93mInstalando dependencias\e[0m\n"
+	printf "\e[1;97mInstalando dependencias\e[0m\n"
 	printf "\n\e[1;91mXterm\e[0m\n"
 	sudo apt-get install -y xterm
 	printf "\n\e[1;34mAircrack-ng\e[0m\n"
 	sudo apt-get install aircrack-ng
+	printf "\n\e[1;93mBully\e[0m\n"
+	sudo apt-get install bully
 	printf "\n\e[1;92mWifite\e[0m\n"
 	sudo apt-get install wifite
 	printf "\n\e[1;96mMDK3\e[0m\n"
